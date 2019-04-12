498f3d7d98bef152f2d48aef5b27ada82c3b6443
OctopiWithHats <sdgranoff@gmail.com>
2019-04-12 10:33:06 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 require 'sqlite3'
 
 
-DB = {:conn => SQLite3::Database.new("db/songs.db")}
-DB[:conn].execute("DROP TABLE IF EXISTS songs")
+DB = {:conn => SQLite3::Database.new("db/songs.db")}#creates the database
+DB[:conn].execute("DROP TABLE IF EXISTS songs")#drops "songs" to avoid error
 
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS songs (
@@ -12,5 +12,5 @@ sql = <<-SQL
   )
 SQL
 
-DB[:conn].execute(sql)
-DB[:conn].results_as_hash = true
+DB[:conn].execute(sql)#create songs table
+DB[:conn].results_as_hash = true #when a SELECT statement is run, it returns a hash with column names as keys rather than an array
